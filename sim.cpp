c5dbe7b76bec669c227eb5cbc67851eca83c7d4e
slothMan <marie.k.petitjean@gmail.com>
2018-03-24 02:05:41 +0000

    uhhhh

--- sim.cpp
+++ sim.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include "MemoryStore.h"
 #include "RegisterInfo.h"
+#include "EndianHelpers.h"
 
 using namespace std;
 
@@ -10,27 +11,31 @@ int main(int argc, char *argv[])
     MemoryStore *mem;
     RegisterInfo reg;
     FILE *file;
+    uint32_t address; 
 
     if (argc != 2) {
-        fprintf(stderr, "%s: Incorrect usage. Provide a MIPS file as an argument.", argv[0]);
+        fprintf(stderr, "%s: Incorrect usage. Provide a MIPS file as an argument.\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     if ((file = fopen(argv[1], "r")) == NULL) {
         perror(argv[0]);
     }
+
+    mem = createMemoryStore();
+    address = 0x0; 
+
     while ( !feof(file)) {
         uint32_t temp;
 
+	fread(&temp, 4, 1, file);
+	mem->setMemValue(address, ConvertWordToBigEndian(temp), WORD_SIZE);
+	address+=0x4; 
     }
-    mem = createMemoryStore();
 
 
 
-
-
-
-    dumpRegisterState(reg);
+    //dumpRegisterState(reg);
     dumpMemoryState(mem);
 
     delete mem;
